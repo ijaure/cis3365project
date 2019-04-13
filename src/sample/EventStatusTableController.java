422c94f6a47307bef449bdcffe408a80d78c974a
rchrist2 <rachelc504@gmail.com>
2019-04-12 19:07:07 -0500

    Completed Event Occurance, Event Status, Venue Contact, and Venue Status Controllers

--- src/sample/EventStatusTableController.java
+++ src/sample/EventStatusTableController.java
@@ -14,62 +14,62 @@ import javafx.stage.Stage;
 import java.sql.*;
 
 
-public class EventOccuranceTableController {
+public class EventStatusTableController {
 
-    public ObservableList<EventOccurance> eoData = FXCollections.observableArrayList();
-    public TableView<EventOccurance> eoTable;
-    public TableColumn<EventOccurance, Number> eoIDCol;
-    public TableColumn<EventOccurance, String> occurNameCol;
+    public ObservableList<EventStatus> eStatusData = FXCollections.observableArrayList();
+    public TableView<EventStatus> esTable;
+    public TableColumn<EventStatus, Number> esIDCol;
+    public TableColumn<EventStatus, String> sNameCol;
 
     public void initialize(){
         //Connect to Database
         Connection c;
 
-        eoIDCol.setCellValueFactory(data -> data.getValue().event_occurance_idProperty());
-        occurNameCol.setCellValueFactory(data -> data.getValue().event_occurance_nameProperty());
+        esIDCol.setCellValueFactory(data -> data.getValue().event_status_idProperty());
+        sNameCol.setCellValueFactory(data -> data.getValue().event_status_nameProperty());
 
-        eoTable.setEditable(true);
+        esTable.setEditable(true);
 
         try{
             c = DBClass.connect();
-            String SQL = "Select * from EVENT_OCCURANCE";
+            String SQL = "Select * from EVENT_STATUS";
 
             ResultSet rs = c.createStatement().executeQuery(SQL);
             while(rs.next()){
-                EventOccurance eo = new EventOccurance();
+                EventStatus es = new EventStatus();
 
                 //set the values for this new Vendor based on what's in the database
-                eo.event_occurance_id.set(rs.getInt("EVENT_OCCURANCE_ID")); //columnLabel should match column name in database
-                eo.event_occurance_name.set(rs.getString("EVENT_OCCURANCE_NAME"));
+                es.event_status_id.set(rs.getInt("EVENT_STATUS_ID")); //columnLabel should match column name in database
+                es.event_status_name.set(rs.getString("EVENT_STATUS_NAME"));
 
-                eoData.add(eo); //add the new Vendor to an observable list
+                eStatusData.add(es); //add the new Vendor to an observable list
             }
-            eoTable.setItems(eoData); //set the table items to the observable list's Vendors
+            esTable.setItems(eStatusData); //set the table items to the observable list's Vendors
             c.close();
         }
         catch(Exception e){
             e.printStackTrace();
-            System.out.println("Error on Building Event Occurance Table Data");
+            System.out.println("Error on Building Event Status Table Data");
         }
 
     }
 
-    public void editEventOccur(){
-        occurNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        occurNameCol.setOnEditCommit(
-                (TableColumn.CellEditEvent<EventOccurance, String> t) ->
+    public void editEventStatus(){
+        sNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        sNameCol.setOnEditCommit(
+                (TableColumn.CellEditEvent<EventStatus, String> t) ->
                         ( t.getTableView().getItems().get(
                                 t.getTablePosition().getRow())
-                        ).setEvent_occurance_name(t.getNewValue())
+                        ).setEvent_status_name(t.getNewValue())
         );
 
     }
 
-    public void saveEventOccurChanges() throws SQLException {
+    public void saveEventStatusChanges() throws SQLException {
         //get the connection
         Connection c = DBClass.connect();
 
-        if(eoTable.getSelectionModel().isEmpty())
+        if(esTable.getSelectionModel().isEmpty())
         {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("No Selection");
@@ -78,13 +78,13 @@ public class EventOccuranceTableController {
             alert.showAndWait();
         }
         else{
-            int row = eoTable.getSelectionModel().getSelectedIndex(); //get the index of the selected row
-            Integer currentID = (Integer) eoIDCol.getCellObservableValue(row).getValue(); //collect the selected vendor's id
+            int row = esTable.getSelectionModel().getSelectedIndex(); //get the index of the selected row
+            Integer currentID = (Integer) esIDCol.getCellObservableValue(row).getValue(); //collect the selected id
 
-            String nameCell = (String) occurNameCol.getCellObservableValue(row).getValue();
+            String nameCell = (String) sNameCol.getCellObservableValue(row).getValue();
 
-            PreparedStatement statement = c.prepareStatement("UPDATE EVENT_OCCURANCE SET EVENT_OCCURANCE_NAME = ? "
-                    + "WHERE EVENT_OCCURANCE_ID ="+currentID);
+            PreparedStatement statement = c.prepareStatement("UPDATE EVENT_STATUS SET EVENT_STATUS_NAME = ? "
+                    + "WHERE EVENT_STATUS_ID ="+currentID);
 
             // set the value of each question mark in the sql statement to the variables above
             // make sure these are in the correct order
@@ -92,16 +92,14 @@ public class EventOccuranceTableController {
             statement.execute();
             c.close();
         }
-
-
     }
 
-    public void deleteEventOccur() throws SQLException {
+    public void deleteEventStatus() throws SQLException {
         //get the connection
         Connection c = DBClass.connect();
         Statement stmt = c.createStatement();
 
-        if(eoTable.getSelectionModel().isEmpty())
+        if(esTable.getSelectionModel().isEmpty())
         {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("No Selection");
@@ -111,15 +109,15 @@ public class EventOccuranceTableController {
         }
         else {
 
-            int row = eoTable.getSelectionModel().getSelectedIndex(); //get the index of the current selection
-            Integer currentID = (Integer) eoIDCol.getCellObservableValue(row).getValue(); //get the id of the selection
+            int row = esTable.getSelectionModel().getSelectedIndex(); //get the index of the current selection
+            Integer currentID = (Integer) esIDCol.getCellObservableValue(row).getValue(); //get the id of the selection
 
             //delete the vendor whose id matches the currently selected vendor's id
-            String SQL = "DELETE FROM EVENT_OCCURANCE WHERE EVENT_OCCURANCE_ID =" + currentID;
+            String SQL = "DELETE FROM EVENT_STATUS WHERE EVENT_STATUS_ID=" + currentID;
 
             stmt.executeUpdate(SQL);
-            eoData.remove(eoTable.getSelectionModel().getSelectedIndex()); //update the observable list
-            eoTable.setItems(eoData); //update the tableview so the deletion shows immediately
+            eStatusData.remove(esTable.getSelectionModel().getSelectedIndex()); //update the observable list
+            esTable.setItems(eStatusData); //update the tableview so the deletion shows immediately
             c.close();
         }
 
@@ -138,16 +136,18 @@ public class EventOccuranceTableController {
         }
     }
 
-    public void openTypeForm(){
+    public void openStatusForm(){
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/New Type Form.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/New Status Form.fxml"));
             Parent root = fxmlLoader.load();
             Stage stage = new Stage();
-            stage.setTitle("New Type");
+            stage.setTitle("New Status");
             stage.setScene(new Scene(root));
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
 }
---
+++ src/sample/VenueContactFormController.java
@@ -0,0 +1,87 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class VenueContactFormController {
+    public ObservableList<Venue> venueData = FXCollections.observableArrayList();
+    public ComboBox<Venue> venueList;
+
+    public TextField fNameInput;
+    public TextField lNameInput;
+    public TextField phoneInput;
+    public TextField emailInput;
+
+    public void initialize(){
+        Connection c;
+
+        try{
+            c = DBClass.connect();
+            String SQL = "SELECT * from VENUE";
+            ResultSet rs = c.createStatement().executeQuery(SQL);
+
+            while(rs.next()){
+                Venue ve = new Venue();
+
+                //assign an ID Name from the database
+                ve.venue_id.set(rs.getInt("VENUE_ID"));
+                ve.venue_name.set(rs.getString("VENUE_NAME"));
+
+                venueData.add(ve); //add these to an observable list
+            }
+            venueList.setItems(venueData); //set the ComboBox values to the observable list
+            c.close();
+        }
+        catch(Exception e){ //catch any exceptions
+            e.printStackTrace();
+            System.out.println("Error on Building Venue Contact Combobox Data");
+        }
+
+    }
+
+    public void addVenueContact() throws SQLException {
+        //get the connection
+        Connection c = DBClass.connect();
+        Statement stmt = c.createStatement();
+
+        //output an error if any of the fields are empty
+        if(venueList.getSelectionModel().isEmpty() ||
+                fNameInput.getText().trim().isEmpty() ||
+                lNameInput.getText().trim().isEmpty() ||
+                phoneInput.getText().trim().isEmpty() ||
+                emailInput.getText().trim().isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Missing Values");
+            alert.setHeaderText("There Are Missing Values");
+            alert.setContentText("Please check that all fields are complete before submitting.");
+            alert.showAndWait();
+        }
+        else {
+            //collect all the values from the textfields
+            Integer venue = venueList.getSelectionModel().getSelectedItem().getVenue_id();
+            String fName = fNameInput.getText();
+            String lName = lNameInput.getText();
+            String phone = phoneInput.getText();
+            String email = emailInput.getText();
+
+
+            //insert all of these values to the db, make sure they are in the same order as in the db
+            String SQL = "INSERT INTO VENUE_CONTACT " + "(FK_VENUE_ID, CONTACT_PERSON_FIRST_NAME, CONTACT_PERSON_LAST_NAME, CONTACT_PERSON_PHONE, CONTACT_PERSON_EMAIL) "
+                    + "VALUES ('" + venue + "', '" + fName + "', '" + lName + "', '" + phone + "', '" + email + "')";
+
+            stmt.executeUpdate(SQL);
+            c.close();
+        }
+
+    }
+
+}
